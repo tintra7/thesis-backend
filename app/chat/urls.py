cc527d5fc09ffb91ff3dc6ce3071412a10224c4c
tintra <tintra17@gmail.com>
2024-09-13 11:21:32 +0700

    update: chatbot

--- app/chat/urls.py
+++ app/chat/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include, re_path
 from . import views
 urlpatterns = [
     re_path(r'^$', views.chat ),
-    path('delete/', views.delete_conversation),
     path('get-titles', views.get_title),
     path('get-data/', views.get_data)
 ]
--- app/chat/views.py
+++ app/chat/views.py
@@ -14,7 +14,7 @@ from django.http import JsonResponse
 from chat.utils import create_response
 
 
-@api_view(['POST', 'GET'])
+@api_view(['POST', 'GET', 'DELETE'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def chat(request):
@@ -23,13 +23,16 @@ def chat(request):
         provided_title = request.GET.get('title')
         user = request.user
         if provided_title:
-            conversation_title = Conversation.objects.get(
-                title=provided_title, user=user)
-            conversation_id = getattr(conversation_title, 'id')
-            ChatObj = ChatMessage.objects.filter(
-                conversation_id=conversation_id).order_by('timestamp')
-            Chat = ChatMessageSerializer(ChatObj, many=True)
-            return JsonResponse(Chat.data, safe=False)
+            try:
+                conversation_title = Conversation.objects.get(
+                    title=provided_title, user=user)
+                conversation_id = getattr(conversation_title, 'id')
+                ChatObj = ChatMessage.objects.filter(
+                    conversation_id=conversation_id).order_by('timestamp')
+                Chat = ChatMessageSerializer(ChatObj, many=True)
+                return JsonResponse(Chat.data, safe=False)
+            except:
+                return JsonResponse({"message": "Does not exist this conversation"}, status=404)
         else:
             return JsonResponse({'error': 'Title not provided'}, status=400)
 
@@ -60,7 +63,15 @@ def chat(request):
             'ai_response': response,
             'title':title
         }, status=201)
-
+    elif request.method == "DELETE":
+        user=request.user
+        title= request.query_params.get('title')
+        try:
+            obj=Conversation.objects.get(user=user, title=title)
+            obj.delete()
+            return JsonResponse({"message": "Deleted succesfully"}, safe=False)
+        except:
+            return JsonResponse({"message": "Does not exist this conversation"}, status=404)
 
 
 # Retriving all conversations of a user ( Titles only )
@@ -76,15 +87,15 @@ def get_title(request):
 
 # Delete a conversation by providing title of conversation
  
-@api_view(['DELETE'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated]) 
-def delete_conversation(request):
-    user=request.user
-    title= request.data.get('title')
-    obj=Conversation.objects.get(user=user, title=title)
-    obj.delete()
-    return JsonResponse("Deleted succesfully", safe=False)
+# @api_view(['DELETE'])
+# @authentication_classes([TokenAuthentication])
+# @permission_classes([IsAuthenticated]) 
+# def delete_conversation(request):
+#     user=request.user
+#     title= request.data.get('title')
+#     obj=Conversation.objects.get(user=user, title=title)
+#     obj.delete()
+#     return JsonResponse("Deleted succesfully", safe=False)
 
   
 @api_view(['POST'])
